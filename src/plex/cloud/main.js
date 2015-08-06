6a8df2371443eb8df81a81a750d239b20ba03994
Dean Gardiner <gardiner91@gmail.com>
2015-08-07 11:20:15 +1200

    Added "plex/core/http" module to replace the Cloud/Server `request` method

--- src/plex/cloud/main.js
+++ src/plex/cloud/main.js
@@ -1,8 +1,7 @@
 define([
     'plex/core/headers',
+    'plex/core/http',
     'plex/cloud/interfaces/registry',
-    'httpinvoke',
-    'when',
 
     // cloud interfaces
     'plex/cloud/interfaces/api/home/root',
@@ -19,17 +18,13 @@ define([
     'plex/cloud/interfaces/pins',
     'plex/cloud/interfaces/root',
     'plex/cloud/interfaces/users'
-], function(Headers, registry, httpinvoke, when) {
+], function(Headers, Http, registry) {
     function Cloud() {
-        this.baseUrl = 'https://plex.tv';
+        this.http = new Http.Client(this, 'https://plex.tv');
         this.headers = new Headers();
 
         this.client_identifier = null;
         this.token = null;
-
-        this.xmlParser = 'dom';
-
-        this._x2js = null;
 
         // Expose interfaces
         registry.expose(this);
@@ -37,63 +32,7 @@ define([
     }
 
     Cloud.prototype.request = function(method, path, config) {
-        var url = this.baseUrl + path,
-            deferred = when.defer(),
-            self = this;
-
-        // Build request
-        config = typeof config !== 'undefined' ? config : {};
-        config.converters = {
-            'xml text': function(value) {
-                return (new XMLSerializer()).serializeToString(value);
-            },
-            'text xml': function(value) {
-                if(self.xmlParser === 'dom') {
-                    return (new DOMParser()).parseFromString(value, 'text/xml');
-                }
-
-                if(self.xmlParser === 'x2js') {
-                    // Check if x2js is available
-                    if (!X2JS) {
-                        throw new Error("Missing X2JS library");
-                    }
-
-                    if(self._x2js === null) {
-                        self._x2js = new X2JS();
-                    }
-
-                    // Parse response
-                    return self._x2js.xml_str2json(value);
-                }
-
-                throw new Error('Unknown "responseType": ' + self.xmlParser);
-            }
-        };
-        config.headers = this.headers.get(config.headers);
-
-        // Set extra headers
-        if(this.client_identifier !== null) {
-            config.headers['X-Plex-Client-Identifier'] = this.client_identifier;
-        }
-
-        if(this.token !== null) {
-            config.headers['X-Plex-Token'] = this.token;
-        }
-
-        // Send request
-        httpinvoke(url, method, config).then(function(response) {
-            // Handle response
-            if(response.statusCode >= 200 && response.statusCode <= 299) {
-                deferred.resolve(response.body);
-            } else {
-                deferred.reject(response.body, response.statusCode, response.headers, null, response.statusText);
-            }
-        }, function(statusText) {
-            // Socket error
-            deferred.reject(null, null, null, null, statusText);
-        });
-
-        return deferred.promise;
+        return this.http.request(method, path, config);
     };
 
     return Cloud;
---
+++ src/plex/core/http.js
@@ -0,0 +1,83 @@
+define([
+    'httpinvoke',
+    'when'
+], function(httpinvoke, when) {
+    function HttpClient(owner, baseUrl) {
+        this.owner = owner;
+        this.baseUrl = baseUrl;
+
+        this.converters = this.buildConverters();
+        this.xmlParser = 'dom';
+
+        this._x2js = null;
+    }
+
+    HttpClient.prototype.buildConverters = function() {
+        var self = this;
+
+        return {
+            'xml text': function(value) {
+                return (new XMLSerializer()).serializeToString(value);
+            },
+            'text xml': function(value) {
+                if(self.xmlParser === 'dom') {
+                    return (new DOMParser()).parseFromString(value, 'text/xml');
+                }
+
+                if(self.xmlParser === 'x2js') {
+                    // Check if x2js is available
+                    if (!X2JS) {
+                        throw new Error("Missing X2JS library");
+                    }
+
+                    if(self._x2js === null) {
+                        self._x2js = new X2JS();
+                    }
+
+                    // Parse response
+                    return self._x2js.xml_str2json(value);
+                }
+
+                throw new Error('Unknown "responseType": ' + self.xmlParser);
+            }
+        };
+    };
+
+    HttpClient.prototype.request = function(method, path, config) {
+        var url = this.baseUrl + path,
+            deferred = when.defer();
+
+        // Build request
+        config = typeof config !== 'undefined' ? config : {};
+        config.converters = this.converters;
+        config.headers = this.headers.get(config.headers);
+
+        // Set extra headers
+        if(this.owner.client_identifier !== null) {
+            config.headers['X-Plex-Client-Identifier'] = this.owner.client_identifier;
+        }
+
+        if(this.owner.token !== null) {
+            config.headers['X-Plex-Token'] = this.owner.token;
+        }
+
+        // Send request
+        httpinvoke(url, method, config).then(function(response) {
+            // Handle response
+            if(response.statusCode >= 200 && response.statusCode <= 299) {
+                deferred.resolve(response.body);
+            } else {
+                deferred.reject(response.body, response.statusCode, response.headers, null, response.statusText);
+            }
+        }, function(statusText) {
+            // Socket error
+            deferred.reject(null, null, null, null, statusText);
+        });
+
+        return deferred.promise;
+    };
+
+    return {
+        Client: HttpClient
+    };
+});
